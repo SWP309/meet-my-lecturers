4d8acde86a3584e6d5a1389ccf482d76eaee6af1
hiep-tm <tranhiep.az2@gmail.com>
2023-10-07 02:51:50 +0700

    Update UserDTO to match with DB (by hiep-tm)

--- src/java/sample/users/UserDTO.java
+++ src/java/sample/users/UserDTO.java
@@ -1,38 +1,30 @@
 package sample.users;
 
 public class UserDTO {
+
     private String userID;
     private String userName;
     private String userEmail;
     private boolean userStatus;
-    private String timetableID;
     private String roleID;
     private String password;
-    private String requestID;
-    private String freeSlotID;
 
     public UserDTO() {
         this.userID = "";
         this.userName = "";
         this.userEmail = "";
         this.userStatus = true;
-        this.timetableID = "";
         this.roleID = "";
         this.password = "";
-        this.requestID = "";
-        this.freeSlotID = "";
     }
 
-    public UserDTO(String userID, String userName, String userEmail, boolean userStatus, String timetableID, String roleID, String password, String requestID, String freeSlotID) {
+    public UserDTO(String userID, String userName, String userEmail, boolean userStatus, String roleID, String password) {
         this.userID = userID;
         this.userName = userName;
         this.userEmail = userEmail;
         this.userStatus = userStatus;
-        this.timetableID = timetableID;
         this.roleID = roleID;
         this.password = password;
-        this.requestID = requestID;
-        this.freeSlotID = freeSlotID;
     }
 
     /**
@@ -92,20 +84,6 @@ public class UserDTO {
     }
 
     /**
-     * @return the timetableID
-     */
-    public String getTimetableID() {
-        return timetableID;
-    }
-
-    /**
-     * @param timetableID the timetableID to set
-     */
-    public void setTimetableID(String timetableID) {
-        this.timetableID = timetableID;
-    }
-
-    /**
      * @return the roleID
      */
     public String getRoleID() {
@@ -132,35 +110,4 @@ public class UserDTO {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    /**
-     * @return the requestID
-     */
-    public String getRequestID() {
-        return requestID;
-    }
-
-    /**
-     * @param requestID the requestID to set
-     */
-    public void setRequestID(String requestID) {
-        this.requestID = requestID;
-    }
-
-    /**
-     * @return the freeSlotID
-     */
-    public String getFreeSlotID() {
-        return freeSlotID;
-    }
-
-    /**
-     * @param freeSlotID the freeSlotID to set
-     */
-    public void setFreeSlotID(String freeSlotID) {
-        this.freeSlotID = freeSlotID;
-    }
-    
-    
 }
-        
